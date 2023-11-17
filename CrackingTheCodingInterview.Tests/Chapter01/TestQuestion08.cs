9aa7912ac3503fa46823990252b2c1f19212f0d0
Connor Mason <cdubyamason@outlook.com>
2023-11-17 00:47:00 -0500

    question 8 chapter 1 done

---
+++ CrackingTheCodingInterview.Tests/Chapter01/TestQuestion08.cs
@@ -0,0 +1,54 @@
+﻿using CrackingTheCodingInterview.Chapter01;
+using FluentAssertions;
+
+namespace CrackingTheCodingInterview.Tests.Chapter01;
+
+public class TestQuestion08
+{
+    private static Question08 Sut => new();
+
+    public static IEnumerable<object[]> MatrixRotationDataOne =>
+        new List<object[]>
+        {
+            // Test Case 1
+            new object[] 
+            { 
+                new List<int>[] 
+                {
+                    new() { 0, 2, 3 },
+                    new() { 4, 5, 6 }, 
+                    new() { 7, 8, 0 } 
+                },
+                new List<int>[]
+                {
+                    new() { 0, 0, 0 },
+                    new() { 0, 5, 0 }, 
+                    new() { 0, 0, 0 } 
+                }
+            },
+            // Test Case 2
+            new object[] 
+            { 
+                new List<int>[] 
+                { 
+                    new() { 8, 0, 6 }, 
+                    new() { 9, 1, 2 }, 
+                    new() { 7, 4, 7 } 
+                },
+                new List<int>[]
+                {
+                    new() { 0, 0, 0 }, 
+                    new() { 9, 0, 2 }, 
+                    new() { 7, 0, 7 }
+                }
+            }
+        };
+
+    [Theory]
+    [MemberData(nameof(MatrixRotationDataOne))]
+    public void ZeroOutColumnAndRowIfValueZero(List<int>[] input, List<int>[] expected)
+    {
+        var result = (int[][])Sut.Run(input);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ CrackingTheCodingInterview/Chapter01/Question08.cs
@@ -0,0 +1,78 @@
+﻿using CrackingTheCodingInterview.Shared;
+
+namespace CrackingTheCodingInterview.Chapter01;
+
+/// <summary>
+/// Write an algorithm such that if an element in an MxN matrix is zero, its entire row and column are set to zero
+/// </summary>
+public class Question08 : Question
+{
+    private static int[][] ZeroOut(List<int>[] input)
+    {
+        int m = input.Length;
+        int n = input[0].Count;
+        var row = new bool[m];
+        var column = new bool[n];
+        var result = new int[m][];
+        // Build the result and if there is a zero value,
+        // set the row and column corresponding to true
+        for (var i = 0; i < m; i++)
+        {
+            result[i] = new int[n];
+            for (var j = 0; j < n; j++)
+            {
+                result[i][j] = input[i][j];
+                if (result[i][j] != 0) continue;
+                
+                row[i] = true;
+                column[j] = true;
+            }
+        }
+        
+        // Iterate through the row boolean array and ZeroOutRow if there is a zero
+        for (var i = 0; i < row.Length; i++)
+        {
+            if (row[i]) ZeroOutRow(result, i);
+        }
+        
+        // Iterate through the column array and zero out if there is a zero
+        for (var j = 0; j < column.Length; j++)
+        {
+            if (column[j]) ZeroOutColumn(result, j);
+        }
+
+        return result;
+    }
+
+    private static void ZeroOutRow(int[][] input, int row)
+    {
+        for (int j = 0; j < input[0].Length; j++)
+        {
+            input[row][j] = 0;
+        }
+    }
+
+    private static void ZeroOutColumn(int[][] input, int column)
+    {
+        for (var i = 0; i < input.Length; i++)
+        {
+            input[i][column] = 0;
+        }
+    }
+    
+    public override object Run(params object[] parameters)
+    {
+        if (parameters == null || parameters.Length == 0 || parameters.Any(p => p is not List<int>))
+        {
+            throw new ArgumentException("Invalid or missing parameters");
+        }
+
+        var input = new List<int>[parameters.Length];
+        for (var i = 0; i < parameters.Length; i++)
+        {
+            input[i] = parameters[i] as List<int> ?? throw new InvalidOperationException();
+        }
+
+        return ZeroOut(input);
+    }
+}
