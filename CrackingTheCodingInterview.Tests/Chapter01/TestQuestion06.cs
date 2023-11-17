216ed29bdf636a1b09019b56b558738929c9b3c6
Connor Mason <cdubyamason@outlook.com>
2023-11-16 20:43:26 -0500

    question 6 completed

---
+++ CrackingTheCodingInterview.Tests/Chapter01/TestQuestion06.cs
@@ -0,0 +1,20 @@
+﻿using CrackingTheCodingInterview.Chapter01;
+using FluentAssertions;
+
+namespace CrackingTheCodingInterview.Tests.Chapter01;
+
+public class TestQuestion06
+{
+    private static Question06 Sut => new();
+
+    [Theory]
+    [InlineData("aabbbcdddde", "a2b3c1d4e1")]
+    [InlineData("aaaaaaaabbbiiilllllllll", "a8b3i3l9")]
+    [InlineData("abcd", "abcd")]
+    [InlineData("connorzzzzzzzzzzzz", "c1o1n2o1r1z12")]
+    public void Compress(string input, string expectedOutput)
+    {
+        var result = (string)Sut.Run(input);
+        result.Should().Be(expectedOutput);
+    }
+}
---
+++ CrackingTheCodingInterview/Chapter01/Question06.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using CrackingTheCodingInterview.Shared;
+
+namespace CrackingTheCodingInterview.Chapter01;
+
+public class Question06 : Question
+{
+    private static string Compress(string input)
+    {
+        var characterCount = 0;
+        char currentCharacter = input[0];
+        var builtString = new StringBuilder();
+        foreach (char c in input)
+        {
+            if (c == currentCharacter) 
+                characterCount++;
+            else
+            {
+                builtString.Append($"{currentCharacter}{characterCount}");
+                characterCount = 1;
+                currentCharacter = c;
+            }
+        }
+        builtString.Append($"{currentCharacter}{characterCount}");
+
+        return builtString.Length > input.Length ? 
+            input : 
+            builtString.ToString();
+    }
+    public override object Run(params object[] parameters)
+    {
+        var input = parameters[0] as string;
+        return Compress(input!);
+    }
+}
