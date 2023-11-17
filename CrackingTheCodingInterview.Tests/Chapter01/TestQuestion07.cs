ba2669778bff7a5584823b27c85f387918ebee1a
Connor Mason <cdubyamason@outlook.com>
2023-11-17 00:10:18 -0500

    question 7 done chapter one

---
+++ CrackingTheCodingInterview.Tests/Chapter01/TestQuestion07.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using CrackingTheCodingInterview.Chapter01;
+
+namespace CrackingTheCodingInterview.Tests.Chapter01;
+
+public class TestQuestion07
+{
+    private static Question07 Sut => new();
+
+    public static IEnumerable<object[]> MatrixRotationDataOne =>
+        new List<object[]>
+        {
+            // Test Case 1
+            new object[] 
+            { 
+                new List<int>[] 
+                {
+                    new() { 1, 2, 3 }, //(0, 0), (0, 1), (0, 2) -> 
+                    new() { 4, 5, 6 }, 
+                    new() { 7, 8, 9 } //(i, j) -> (j, n - i - 1
+                },
+                new List<int>[]
+                {
+                    new() { 7, 4, 1 }, // (0, 2), (1, 2), (2, 2)
+                    new() { 8, 5, 2 }, 
+                    new() { 9, 6, 3 }
+                }
+            },
+            // Test Case 2
+            new object[] 
+            { 
+                new List<int>[] 
+                { 
+                    new() { 8, 0, 6 }, 
+                    new() { 9, 1, 2 }, 
+                    new() { 7, 4, 7 } 
+                },
+                new List<int>[]
+                {
+                    new() { 7, 9, 8 }, 
+                    new() { 4, 1, 0 }, 
+                    new() { 7, 2, 6 }
+                }
+            }
+        };
+
+    [Theory]
+    [MemberData(nameof(MatrixRotationDataOne))]
+    public void ReverseImage(List<int>[] input, List<int>[] expected)
+    {
+        var result = (int[][])Sut.Run(input);
+        result.Should().BeEquivalentTo(expected);
+    }
+}
---
+++ CrackingTheCodingInterview/Chapter01/Question07.cs
@@ -0,0 +1,48 @@
+﻿using CrackingTheCodingInterview.Shared;
+
+namespace CrackingTheCodingInterview.Chapter01;
+
+/// <summary>
+/// Given an image represented by an NxN matrix, where each pixel
+/// in the image is four bytes, write a method to rotate the image
+/// by 90 degrees. Can you do this in place?
+/// </summary>
+public class Question07 : Question
+{
+    private static int[][] RotateImageByNinetyDegrees(List<int>[] input)
+    {
+        int n = input.Length;
+        var result = new int[n][];
+
+        for (var i = 0; i < n; i++)
+        {
+            result[i] = new int[n];
+        }
+
+        for (var i = 0; i < n; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                result[j][n - i - 1] = input[i][j];
+            }
+        }
+
+        return result;
+    }
+
+    public override object Run(params object[] parameters)
+    {
+        if (parameters == null || parameters.Length == 0 || parameters.Any(p => p is not List<int>))
+        {
+            throw new ArgumentException("Invalid or missing parameters");
+        }
+
+        var input = new List<int>[parameters.Length];
+        for (var i = 0; i < parameters.Length; i++)
+        {
+            input[i] = parameters[i] as List<int> ?? throw new InvalidOperationException();
+        }
+
+        return RotateImageByNinetyDegrees(input);
+    }
+}
